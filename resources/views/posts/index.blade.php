62ad2652b9e5ce52805dde0bb131f2e79d944b33
KurinMaron <22fs728g@shinshu-u.ac.jp>
2023-01-30 19:30:26 +0900

    ブログ投稿一覧の表示

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!-- app()->getLocale() はconfig/app.phpのlocaleに指定されたlangに従う -->
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- rel:relation stylesheet:見栄え情報を記述したファイル -->
+    </head>
+    
+    <body>
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>{{ $post->title }}</h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+    </body>
+</html>
