6628198f43154c03692dadbc7717569b0208d546
KurinMaron <22fs728g@shinshu-u.ac.jp>
2023-02-24 19:53:54 +0900

    コミットテスト 2文目

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -20,7 +20,21 @@
                     </h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
+                <form action="/posts/{{ $post->id }} id="form_{{ $post->id }} method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                </form>
             @endforeach
+            <script>
+                function deletePost(id) {
+                    'use strict'
+                    
+                    if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                        document.getElementById(`form_${id}`).submit();
+                    }
+                }
+            </script>
         </div>
         <div class='paginate'>
             {{ $posts->links() }}
